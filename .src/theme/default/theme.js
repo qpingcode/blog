cbf672db724f7511bb56cac3853334d070be4f7d
qpingcode <hi@qping.me>
2019-06-06 17:29:59 +0800

    style modify

--- .src/theme/default/theme.js
+++ .src/theme/default/theme.js
@@ -1,28 +1,30 @@
 (function(){
 	// 切换样式
+	var defaultColor = $('#theme-css-dark').length > 0 ? "dark" : "white";
 	var currentColor = $.getCookie("color") ? $.getCookie("color") : "dark";
 	console.log("[theme info] name:default, color:" + currentColor)
 
-	if(currentColor == 'white'){
-        loadColor('white');
+	if(currentColor != defaultColor){
+        switchColor();
 	}
 
 	$("#lightBtn").on("click", function(){
-		loadColor(currentColor == "dark" ? "white" : "dark")
+		switchColor();
 	})
 
-	function loadColor(color){
-		if(color == "dark"){
-            var link = document.createElement('link');
+	function switchColor(){
+		if($('#theme-css-dark').length > 0){
+			$('#theme-css-dark').remove();
+			currentColor = "white";
+		}else{
+ 			var link = document.createElement('link');
 		    link.type = 'text/css';
 		    link.id = "theme-css-dark";
 		    link.rel = 'stylesheet';
 		    link.href = '/theme/default/dark.css';
 		    document.getElementsByTagName("head")[0].appendChild(link);
-		}else{
-			$('#theme-css-dark').remove();
+			currentColor = "dark";
 		}
-		currentColor = color;
 		$.setCookie("color", currentColor, {path:"/"});
 	}
 
